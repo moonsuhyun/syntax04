f74a2994045e6b6e46ad908602510229e1dd7e26
moonsuhyun <moon@itc.ac.kr>
2021-09-19 00:13:01 +0900

    v0.4

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-//ver 0.3
+//ver 0.4
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -19,6 +19,7 @@ func main() {
 		if number%i == 0 {
 			isPrime = false
 		}
+		fmt.Print(i, " ")
 	}
 
 	if isPrime {
@@ -28,6 +29,37 @@ func main() {
 	}
 
 }
+
+//package main
+//
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+//
+////ver 0.3
+//func main() {
+//	seed := time.Now().Unix()
+//	rand.Seed(seed)
+//
+//	isPrime := true
+//	number := rand.Intn(150) + 2
+//	fmt.Println("임의로 추출된 수 : ", number)
+//
+//	for i := 2; i < number; i++ {
+//		if number%i == 0 {
+//			isPrime = false
+//		}
+//	}
+//
+//	if isPrime {
+//		fmt.Println(number, "은 소수입니다.")
+//	} else {
+//		fmt.Println(number, "은 소수가 아닙니다.")
+//	}
+//
+//}
 
 //package main
 //
