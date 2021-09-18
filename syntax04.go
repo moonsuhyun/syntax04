8c36e42b4e65ebaabf4af55ef836f04ff161f3bf
moonsuhyun <moon@itc.ac.kr>
2021-09-19 00:02:03 +0900

    v0.1

---
+++ syntax04.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//ver 0.1
+func main()  {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	count := 0
+	number := rand.Intn(150) + 2
+	fmt.Println("임의로 추출된 수 : ", number)
+
+	for i := 1; i <= number; i++ {
+		if number % i == 0 {
+			count++
+		}
+	}
+
+	if count == 2 {
+		fmt.Println(number, "은 소수입니다.")
+	} else {
+		fmt.Println(number, "은 소수가 아닙니다.")
+	}
+
+}
+
+//package main
+//
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+//
+//func main()  {
+//	seed := time.Now().Unix()
+//	rand.Seed(seed)
+//
+//	for i := 1; i < 6; i++ {
+//		dice := rand.Intn(6)+1
+//		fmt.Println(dice)
+//	}
+//}
